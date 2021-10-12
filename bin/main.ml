9b20038933ed624a07f9f687efa6bdcba53cb5cd
Ben Kogan <brk57@cornell.edu>
2021-10-12 00:41:56 -0400

    Experimenting with pretty printing a board. Works pretty good if you ask me :)

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,53 @@
 open Chess
 open State
 open Game
+open Boards
+open Helper
+
+let get_piece_text color = function
+  | Pawn -> if color = White then "♟︎" else "♙"
+  | Knight -> if color = White then "♞" else "♘"
+  | Bishop -> if color = White then "♝" else "♗"
+  | Rook -> if color = White then "♜" else "♖"
+  | Queen -> if color = White then "♛" else "♕"
+  | King -> if color = White then "♚" else "♔"
+
+let rec row_major arr bd row =
+  if row = 8 then ()
+  else
+    let rec build_row index =
+      (match bd.(index).(row) with
+      | None -> arr.(row).(index) <- " "
+      | Some (piece_color, soldier_type) ->
+          arr.(row).(index) <- get_piece_text piece_color soldier_type);
+      if index = 7 then () else build_row (index + 1);
+      ()
+    in
+    build_row 0;
+    row_major arr bd (row + 1);
+    ()
+
+let rec print_board board row =
+  print_endline "";
+  print_string "|";
+  if row = -1 then ()
+  else
+    let rec print_row index =
+      print_string (board.(row).(index) ^ " |");
+      if index = 7 then () else print_row (index + 1);
+      ()
+    in
+    print_row 0;
+    print_board board (row - 1);
+    ()
+
+let pretty_print () bd =
+  let bd = board_to_array bd in
+  let board = Array.make_matrix 8 8 "" in
+  row_major board bd 0;
+  print_board board 7
+
+let () = pretty_print () starting_board
 
 let data_dir_prefix = "data" ^ Filename.dir_sep
 
