51bed34b8e77e25ecbdf1e358c16700ca41004cc
Ben Kogan <brk57@cornell.edu>
2021-11-15 10:49:09 -0500

    Terminating nodes and documentation

--- src/engine.ml
+++ src/engine.ml
@@ -1,6 +1,8 @@
 open State
 open Game
 
+(** [eval bd color] is the static evaluation of [bd] for the player with
+    [color] pieces. Is the difference in material count. *)
 let eval bd color =
   let piece_value = function
     | Some (c, Pawn) -> if c = color then 10. else -10.
@@ -14,11 +16,31 @@ let eval bd color =
   let sum acc p = acc +. piece_value p in
   List.fold_left sum 0. bd'
 
+(** [update_states pl opp mv turn] is the updated states of [pl] and
+    [opp] with [mv] played. If [turn], then this move was made by [pl],
+    otherwise this move was made by [opp]. *)
 let update_states pl opp mv turn =
   let pl' = if turn then play_move pl mv else receive_move pl mv in
   let opp' = if turn then receive_move opp mv else play_move opp mv in
   (pl', opp')
 
+(** [result pl opp max] is the value of the current state of the game
+    relative to [pl]. If [pl] is checkmating or stalemating, then this
+    is [Some infinity] or [Some 0] respectively. If [opp] is checkmating
+    or stalemating, then this is [Some neg_infinity] or [Some 0]
+    respectively. [max] determines which side the result is for. *)
+let result pl opp max =
+  match (pl.moves, opp.moves) with
+  | [], _ :: _ when pl.king_in_check && max -> Some neg_infinity
+  | [], _ :: _ when (not pl.king_in_check) && max -> Some 0.
+  | _ :: _, [] when opp.king_in_check && not max -> Some infinity
+  | _ :: _, [] when (not opp.king_in_check) && not max -> Some 0.
+  | _, _ -> None
+
+(** [minimax pl opp depth max move first] is maximizing move for [pl] if
+    [max] or the minimizing move for [pl] if [not max], searching to
+    [depth]. [move] is the first move played by [pl] in some path, with
+    this move determined when [first] is true. *)
 let rec minimax pl opp depth max move first =
   let step acc mv =
     let pl', opp' = update_states pl opp mv max in
@@ -26,15 +48,13 @@ let rec minimax pl opp depth max move first =
     let mv = if first then mv else move in
     choose acc (minimax pl' opp' (depth - 1) (not max) mv false)
   in
-  if depth = 0 then (eval pl.game_state.board pl.game_state.color, move)
-  else if max then List.fold_left step (neg_infinity, move) pl.moves
-  else List.fold_left step (infinity, move) opp.moves
-
-(*let next_move pl opp = let eval_move mv = print_endline "Evaluated one
-  move"; let pl', opp' = update_states pl opp mv true in minimax pl'
-  opp' 2 false in let evals = List.map eval_move pl.moves in let map =
-  List.combine evals pl.moves in List.sort compare map |> List.rev |>
-  List.hd |> snd*)
+  match result pl opp max with
+  | Some v -> (v, move)
+  | None ->
+      if depth = 0 then
+        (eval pl.game_state.board pl.game_state.color, move)
+      else if max then List.fold_left step (neg_infinity, move) pl.moves
+      else List.fold_left step (infinity, move) opp.moves
 
 let next_move pl opp =
   snd (minimax pl opp 3 true ((-1, -1), (-1, -1)) true)
